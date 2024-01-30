69d45793fb71b1ea53315222db7844c748205e5c
omoma ig <omomaighawosa440@gmail.com>
2024-01-30 11:01:22 +0100

    resolve models branch changes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { UserDetails } from '../../models/User-details';
+import { User } from '../models/Users';
+import { UserDetails } from '../models/UserDetails';
 
 @Injectable({
   providedIn: 'root',
