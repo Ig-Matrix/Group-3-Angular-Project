b482b4ea495809b44bd20cb12ca13d5a8681eed4
Ekujumi <ekujumitolu1197@gmail.com>
2024-01-31 11:19:53 +0100

    added card component and userlist component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../models/Data';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
-export class CardComponent {
+export class CardComponentComponent {
+ 
+  @Input()
+  user: User | undefined;
 
+  constructor() {}
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, FormsModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
+  title = 'User List';
+  users: User[] = [];
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.userService
+      .getUsers()
+      .subscribe((response) => (this.users = response));
+  }
+  
 }
