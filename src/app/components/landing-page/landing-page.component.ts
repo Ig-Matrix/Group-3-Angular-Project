b8625aae34b54dc9eb92140f3c10f68c1d4ce53c
Moongenevieve <fsamson5@gmail.com>
2024-02-01 12:22:03 +0100

    Worked on the landing page animation

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLongArrowAltRight, faLongArrowRight, faUser } from '@fortawesome/free-solid-svg-icons';
 
+
 @Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [FontAwesomeModule],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+    selector: 'app-landing-page',
+    standalone: true,
+    templateUrl: './landing-page.component.html',
+    styleUrl: './landing-page.component.css',
+    imports: [FontAwesomeModule]
 })
 export class LandingPageComponent {
   faLongArrowRight = faLongArrowAltRight;
   faUser= faUser;
+
+
 }
